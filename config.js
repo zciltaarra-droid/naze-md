efb299b212dd79c8b89c2bee901bb60d225ce707
zciltaarra-droid <zciltaarra@gmail.com>
2025-08-16 05:24:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.owner = ['6283898880398']
+global.ownernomer = "6283898880398"
+global.premium = ['6283898880398']
+global.packname = 'Sticker By graceyy'
+global.author = 'graceyy'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -77,3 +77,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+
